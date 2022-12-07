d56fad21ecb87a6a4c6902e27efcd651afaf15e6
saraalbogami <saalbogami22@twq.idserve.net>
2022-12-07 09:32:11 +0300

    Initial Commit

---
+++ WalkIt/WalkItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkItApp.swift
+//  WalkIt
+//
+//  Created by sara ayed albogami on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct WalkItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
